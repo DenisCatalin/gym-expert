abfce3ced1772177ceffe798b37bde77ad4a4c43
DenisCatalin <deniscotecata@gmail.com>
2022-12-17 16:12:46 +0200

    About page

--- pages/about.jsx
+++ pages/about.jsx
@@ -19,15 +19,15 @@ const About = () => {
   const [scrollValue, setScrollValue] = useState(0);
   const router = useRouter();
 
-  // useEffect(() => {
-  //   const onScroll = e => {
-  //     setScrollValue(e.target.documentElement.scrollTop);
-  //   };
+  useEffect(() => {
+    const onScroll = e => {
+      setScrollValue(e.target.documentElement.scrollTop);
+    };
 
-  //   window.addEventListener("scroll", onScroll);
+    window.addEventListener("scroll", onScroll);
 
-  //   return () => window.removeEventListener("scroll", onScroll);
-  // }, []);
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
 
   const scrollToTop = () => {
     window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
