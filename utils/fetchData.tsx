5a2c4a786c10dbbd802ff59f09e4c5ea26f77eba
DenisCatalin <deniscotecata@gmail.com>
2023-03-09 14:51:47 +0200

    No more request for messages

--- utils/fetchData.tsx
+++ utils/fetchData.tsx
@@ -1,5 +1,6 @@
 export default async function fetchData(endpoint: string, options?: Object) {
   const res = await fetch(endpoint, options);
   const data = await res.json();
+  console.log("S-a facut request");
   return data;
 }
