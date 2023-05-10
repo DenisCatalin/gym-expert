e17276a213e7ed5f1b2506dea730a6f8f7574e6b
DenisCatalin <deniscotecata@gmail.com>
2023-05-10 17:04:08 +0300

    Ce se intampla?

--- pages/api/userDetails.ts
+++ pages/api/userDetails.ts
@@ -1,14 +1,15 @@
 import { getUserDetailsQuery } from "../../lib/db/hasura";
 import type { NextApiRequest, NextApiResponse } from "next";
 // @ts-ignore
-import jwt from "jsonwebtoken";
+import jwt, { JwtPayload } from "jsonwebtoken";
 
 export default async function getUserDetails(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === "GET") {
     try {
-      const token = req ? req.cookies?.token : null;
-      const decodedToken = jwt.verify(token, process.env.SECRET_KEY_HASURA);
-      const userDetails = await getUserDetailsQuery(token, decodedToken.issuer);
+      const token = req && typeof req.cookies?.token === 'string' ? req.cookies.token : '';
+      const secretKey = process.env.SECRET_KEY_HASURA || '';
+      const decodedToken = jwt.verify(token, secretKey) as JwtPayload;
+      const userDetails = await getUserDetailsQuery(token, decodedToken?.issuer || '');
 
       res.send({ done: true, userDetails });
     } catch (error) {
