68e3745fc544fbaa751023bbeecf4c0efb173730
DenisCatalin <deniscotecata@gmail.com>
2023-05-10 17:07:25 +0300

    Lasam asa

--- pages/_app.js
+++ pages/_app.js
@@ -7,8 +7,6 @@ import store from "../redux/store";
 import Header from "../components/Header/Header.c";
 import { useRouter } from "next/router";
 import CustomSnackbar from "../components/Snackbar/Snackbar.c";
-import MusicPlayerButton from "../components/MusicPlayer/MusicPlayerButton.c";
-import MusicPlayer from "../components/MusicPlayer/MusicPlayer.c";
 
 function MyApp({ Component, pageProps }) {
   const [didToken, setDidToken] = useState("");
@@ -21,9 +19,7 @@ function MyApp({ Component, pageProps }) {
       <cropContext.Provider value={{ cropImage, setCropImage }}>
         <Provider store={store}>
           <Header sticky={router.pathname === "/about" ? true : false} />
-          <MusicPlayer />
           <Component {...pageProps} />
-          <MusicPlayerButton />
           <CustomSnackbar />
         </Provider>
       </cropContext.Provider>
